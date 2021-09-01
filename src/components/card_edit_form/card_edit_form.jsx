345750fb26419c0690f25d9472ba3704b784f4b7
Createhb21 <alsghk9701@gmail.com>
2021-09-01 17:27:30 +0900

    add loading spinner for file input

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -10,18 +10,9 @@ const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
   const emailRef = useRef();
   const messageRef = useRef();
 
-  const {
-    name,
-    company,
-    title,
-    email,
-    message,
-    theme,
-    fileName,
-    fileURL,
-  } = card;
+  const { name, company, title, email, message, theme, fileName } = card;
 
-  const onFileChange = file => {
+  const onFileChange = (file) => {
     updateCard({
       ...card,
       fileName: file.name,
@@ -29,7 +20,7 @@ const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
     });
   };
 
-  const onChange = event => {
+  const onChange = (event) => {
     if (event.currentTarget == null) {
       return;
     }
@@ -97,7 +88,7 @@ const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
         onChange={onChange}
       />
       <div className={styles.fileInput}>
-        <FileInput name={name} onFileChange={onFileChange} />
+        <FileInput name={fileName} onFileChange={onFileChange} />
       </div>
       <Button name="Delete" onClick={onSubmit} />
     </form>
